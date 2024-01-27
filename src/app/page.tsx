04a7e9ef8da624807582fc2e18a401b78b3c6989
Denny_Kate <dennykate22@gmail.com>
2024-01-27 21:54:23 +0630

    feat: recently added section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,25 @@
-import { Banner, BestSeller, Hero, InfoSection, PopularCategories } from "@/components/home";
+import {
+  Banner,
+  BestSeller,
+  Hero,
+  InfoSection,
+  PopularCategories,
+  RecentlyAdded,
+} from "@/components/home";
 
 const Home = () => {
-
   return (
     <>
       <Hero />
       <div className="sm:px-4 px-2">
         <InfoSection />
         <BestSeller />
-        <PopularCategories/>
-        <Banner/>
+        <PopularCategories />
+        <Banner />
+        <RecentlyAdded />
       </div>
     </>
   );
-
 };
 
 export default Home;
--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -2,4 +2,5 @@ export { default as Hero } from "./hero/Hero";
 export { default as InfoSection } from "./info/InfoSection";
 export { default as BestSeller } from "./bestSeller/BestSeller";
 export { default as PopularCategories } from "./popularCategories/PopularCategories";
+export { default as RecentlyAdded } from "./recentlyAdded/RecentlyAdded";
 export { default as Banner } from "./banner/BannerSection";
