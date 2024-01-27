599762d393ae64afe9f0119e0c10d84355ef5dc5
Denny_Kate <dennykate22@gmail.com>
2024-01-27 20:08:21 +0630

    feat: product details

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
 export { default as Navbar } from "./navbar/Navbar";
 export { default as ProductCard } from "./productCard/ProductCard";
+export { default as ProductDetails } from "./productDetails/ProductDetails";
+export { default as Ratings } from "./Ratings";
---
+++ src/data/productDetailsData.ts
@@ -0,0 +1,11 @@
+const productDetailsData = {
+  images: [
+    "https://ecolife.posthemes.com/demo1/95-home_default/crispy-mixed-fruit-chips.jpg",
+    "https://ecolife.posthemes.com/demo1/96-home_default/crispy-mixed-fruit-chips.jpg",
+    "https://ecolife.posthemes.com/demo1/94-home_default/crispy-mixed-fruit-chips.jpg",
+    "https://ecolife.posthemes.com/demo1/93-home_default/crispy-mixed-fruit-chips.jpg",
+  ],
+  variants: ["100g", "200g", "300g", "400g"],
+};
+
+export default productDetailsData;
