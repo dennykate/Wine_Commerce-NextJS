af8c0a8bc2e90a1c207d06f5e2cc16e11c105719
Denny_Kate <dennykate22@gmail.com>
2024-01-27 12:39:52 +0630

    feat: navbar init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 const Home = () => {
-  return <div className="bg-black min-h-screen w-full"></div>;
+  return (
+    <div className="bg-black min-h-screen w-full text-white">Hello Worlds</div>
+  );
 };
 
 export default Home;
