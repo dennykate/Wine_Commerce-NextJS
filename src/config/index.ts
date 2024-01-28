c76ee1b1f18c3ffa49881741e70ee00991e36da4
Denny_Kate <dennykate22@gmail.com>
2024-01-28 08:58:05 +0630

    feat: add meta data

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,7 @@
-const config = {};
+const config = {
+  mainTitle: "Fresh Herbs - Nature's Finest Fresh Herb Emporium",
+  mainDescription:
+    "Explore our bountiful selection of premium fresh herbs, sourced directly from local growers. Elevate your culinary creations with the vibrant flavors of nature's finest aromatic and flavorful herbs.",
+};
 
 export default config;
